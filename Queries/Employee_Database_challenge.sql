3101f8d4f6a2362bf62e29a47a6c0115c67f13e4
nebil2016 <100035063+nebil2016@users.noreply.github.com>
2022-04-11 10:39:48 -0400

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,7 +1,7 @@
 --1. Retirement Titles table
 SELECT e.emp_no, e.first_name, e.last_name,
        ti.title, ti.from_date, ti.to_date 
-INTO retirement_titles
+-- INTO retirement_titles
 FROM employees as e
 INNER JOIN Titles as ti
 ON(e.emp_no=ti.emp_no)
@@ -12,7 +12,7 @@ ORDER BY e.emp_no;
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no, rt.first_name,
 rt.last_name,
 rt.title
-INTO unique_titles
+-- INTO unique_titles
 FROM retirement_titles AS rt
 WHERE rt.to_date='9999-01-01'
 ORDER BY rt.emp_no , rt.to_date DESC;
